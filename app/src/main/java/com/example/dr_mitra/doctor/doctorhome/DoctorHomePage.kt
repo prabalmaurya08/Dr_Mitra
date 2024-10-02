ed321621cd7233b17f07e6e31d67a929e28026d1
prabal <prabal1513@gmail.com>
2024-10-02 20:05:40 +0530

    doctorpage

--- app/src/main/java/com/example/dr_mitra/doctor/doctorhome/DoctorHomePage.kt
+++ app/src/main/java/com/example/dr_mitra/doctor/doctorhome/DoctorHomePage.kt
@@ -5,16 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.dr_mitra.R
 import com.example.dr_mitra.databinding.FragmentDoctorHomePageBinding
 
 
 class DoctorHomePage : Fragment() {
     private lateinit var binding: FragmentDoctorHomePageBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerButton:ImageButton
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -23,8 +28,18 @@ class DoctorHomePage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding=FragmentDoctorHomePageBinding.inflate(layoutInflater)
+
+        drawerLayout=binding.doctorDrawerLayout
+        drawerButton=binding.doctorDrawerButton
+
+        setUpDrawer()
         // Inflate the layout for this fragment
         return binding.root
+    }
+    private fun setUpDrawer(){
+        drawerButton.setOnClickListener {
+            drawerLayout.open()
+        }
     }
 
 
